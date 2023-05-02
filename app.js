1bdb67da095b943aec175473941d1a479a7e7516
princehabeeb <habiy3400@gmail.com>
2023-05-02 14:13:25 +0200

    new

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ function Menu(e){
      :( e.name = 'menu', list.classList.remove('top-[80px]'),
      list.classList.remove('opacity-100'));
 }
+
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+    // console.log(window.pageYOffset);
+    if ( window.pageYOffset > 200 ){
+        nav.classList.add('nav-dark')
+    }else{
+        nav.classList.remove('nav-dark')
+    }
+})
